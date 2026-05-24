378d7bda523b3fcc904cfe08e2c0074d6d93b636
Dmitrii722 <168808221+Dmitrii722@users.noreply.github.com>
2026-05-24 15:00:32 +0500

    Delete src directory

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 40, color: 'red', fontSize: 24 }}>
-      Если вы видите этот текст — Vite работает. Проблема в секциях или Lenis/GSAP.
-    </div>
-  );
-}
