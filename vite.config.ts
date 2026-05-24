a01d056cae5d9c66ad265070b22bd0d8f60f68ef
Dmitrii722 <168808221+Dmitrii722@users.noreply.github.com>
2026-05-24 14:51:41 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+// vite.config.ts
+import path from 'path'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-});
+})
